3f1086c72d8cc038ac03a7d50b8658aa0768f4ad
Zhao Min <sebzhaomin@gmail.com>
2013-04-07 19:59:50 +0200

    Setup project

---
+++ ComputerVision/CvConsole/CvConsole/MainWindow.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace CvConsole
+{
+    public partial class MainWindow : Form
+    {
+        private ResourceParser mainWindowParser=new ResourceParser("MainWindow","en-US");
+        private Dictionary<string, Control> navigationDict=new Dictionary<string, Control>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Initialize and store navigation views in a dictionary
+            navigationDict.Add("Startup", new StartupWindow());
+            navigationDict.Add("Monitors", new MonitorsWindow());
+            navigationDict.Add("Models", new ModelsWindow());
+            navigationDict.Add("Settings", new SettingsWindow());
+            // Set all controls dock to fill
+            foreach (Control control in navigationDict.Values)
+                control.Dock = DockStyle.Fill;
+        }
+
+        private void toolS_NavBar_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            // Reset style on all navigation items
+            foreach (ToolStripItem item in toolS_NavBar.Items)
+                item.Font = new Font(item.Font.FontFamily, mainWindowParser.GetData<Single>("NavItemFontSize"),FontStyle.Regular);
+            // Set style on the selected navigation item
+            e.ClickedItem.Font = new Font(e.ClickedItem.Font.FontFamily, mainWindowParser.GetData<Single>("SelectedNavItemFontSize"), FontStyle.Bold);
+            // Navigate to selected view
+            panel_MainContainer.Controls.Clear();
+            panel_MainContainer.Controls.Add(navigationDict[e.ClickedItem.Text]);
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            // Set initial view to start up
+            panel_MainContainer.Controls.Add(navigationDict["Startup"]);
+
+        }
+    }
+}
---
+++ ComputerVision/CvConsole/CvConsole/NavigationControls/ModelsWindow.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CvConsole
+{
+    partial class ModelsWindow
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLP_Main = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLP_Main
+            // 
+            this.tableLP_Main.ColumnCount = 2;
+            this.tableLP_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_Main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLP_Main.Location = new System.Drawing.Point(0, 0);
+            this.tableLP_Main.Name = "tableLP_Main";
+            this.tableLP_Main.RowCount = 2;
+            this.tableLP_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_Main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_Main.Size = new System.Drawing.Size(515, 482);
+            this.tableLP_Main.TabIndex = 0;
+            // 
+            // ModelsWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLP_Main);
+            this.Name = "ModelsWindow";
+            this.Size = new System.Drawing.Size(515, 482);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLP_Main;
+    }
+}
